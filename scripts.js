198fbbfbda13fbd52a13cca797c491e3ba450488
mateus1020 <mateusd823@gmail.com>
2024-10-18 18:53:52 -0300

    Primeiro Commit - Projeto Mario

---
+++ scripts.js
@@ -0,0 +1,58 @@
+
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao() {
+  form.style.left = "50%"
+  form.style.transform ="translateX(-50%)"
+  mascara.style.visibility = "visible"
+}
+
+function mascForm() {
+  form.style.left = "-290px"
+  form.style.transform ="translateX(0)"
+  mascara.style.visibility = "hidden"
+}
+
+function botaoFaleconosco() {
+  form.style.left = "50%"
+  form.style.transform ="translateX(-50%)"
+  mascara.style.visibility = "visible"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const numero1 = 200
+const numero2 = 300
+
+
+const numerototal = numero1 + numero2
+
+console.log (numerototal)
+
+
+function soma() {
+  const somatoria = numero1 + numero2
+  console.log(somatoria)
+}
+soma () */
